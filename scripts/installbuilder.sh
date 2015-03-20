9a14adabc89e3ced1605d61c34e214bf5744aa50
Matthew Aguirre <matt.aguirre@gmail.com>
2015-03-20 10:34:59 -0400

    Fixes to change to maven Added left out licenseheader.txt Fixed installbuilder script.

--- scripts/installbuilder.sh
+++ scripts/installbuilder.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "/opt/installbuilder-8.6.0/bin/build TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2"
+echo "/opt/installbuilder-8.6.0/bin/builder TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2"
 
 if [ -d '/opt/installbuilder-8.6.0' ]; then
-    #/opt/installbuilder-8.6.0/bin/build TorgoInstallBuilder.xml windows --setvars project.outputDirectory=/${installer.output.dir} project.version=${version}
-    /opt/installbuilder-8.6.0/bin/build TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2
+    /opt/installbuilder-8.6.0/bin/builder TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2
 else
     echo '/opt/installbuilder-8.6.0 not found'
 fi
