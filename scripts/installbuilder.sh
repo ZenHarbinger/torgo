d2b8f8912921320676e3a5f642d259a89c659dcc
Matthew Aguirre <matt.aguirre@gmail.com>
2015-03-20 10:39:13 -0400

    Another fix to installbuilder script.

--- scripts/installbuilder.sh
+++ scripts/installbuilder.sh
@@ -2,7 +2,7 @@
 echo "/opt/installbuilder-8.6.0/bin/builder TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2"
 
 if [ -d '/opt/installbuilder-8.6.0' ]; then
-    /opt/installbuilder-8.6.0/bin/builder TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2
+    /opt/installbuilder-8.6.0/bin/builder build TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2
 else
     echo '/opt/installbuilder-8.6.0 not found'
 fi
