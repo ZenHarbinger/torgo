1241d68ebb4a6bcc0ef266072c84d69a56375b28
Matthew Aguirre <matt.aguirre@gmail.com>
2015-03-20 10:41:28 -0400

    Updated installbuider setup to correct output directory.

--- scripts/installbuilder.sh
+++ scripts/installbuilder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "/opt/installbuilder-8.6.0/bin/builder TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2"
+echo "/opt/installbuilder-8.6.0/bin/builder build TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2"
 
 if [ -d '/opt/installbuilder-8.6.0' ]; then
     /opt/installbuilder-8.6.0/bin/builder build TorgoInstallBuilder.xml windows --setvars project.outputDirectory=$1 project.version=$2
