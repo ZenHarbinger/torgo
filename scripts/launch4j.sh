4277ed14b3bf6ff381ee050248a898d5365f2988
Matthew Aguirre <matt.aguirre@gmail.com>
2015-12-29 15:48:51 -0500

    Set to version 1.0 Added desktop and icon. Created a debian file for Linux using jdeb

--- scripts/launch4j.sh
+++ scripts/launch4j.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo "java -jar /opt/launch4j/launch4j.jar $1"
 
-if [ -d '/opt/launch4j/' ]; then
+if [ -d $2 ]; then
+	echo "java -jar $2/launch4j.jar $1"
+    java -jar $2/launch4j.jar $1
+elif [ -d '/opt/launch4j/' ]; then
+	echo "java -jar /opt/launch4j/launch4j.jar $1"
     java -jar /opt/launch4j/launch4j.jar $1
 else
     echo "/opt/launch4j not found"
