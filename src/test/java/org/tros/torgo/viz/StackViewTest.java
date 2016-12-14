7de14e976265ed447e94f0cecbe79d8159d6411c
Matthew Aguirre <matt.aguirre@gmail.com>
2016-12-14 12:24:18 -0500

    Added test for the StackView functionality. Not 100% coverage, but a lot.

---
+++ src/test/java/org/tros/torgo/viz/StackViewTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2015 Matthew Aguirre
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tros.torgo.viz;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.tros.logo.DynamicLogoController;
+import org.tros.logo.LogoControllerTest;
+import static org.junit.Assert.*;
+import org.tros.logo.swing.LogoMenuBar;
+import org.tros.torgo.TorgoInfo;
+import org.tros.torgo.TorgoToolkit;
+import org.tros.torgo.interpreter.CodeBlock;
+import org.tros.torgo.interpreter.InterpreterListener;
+import org.tros.torgo.interpreter.Scope;
+import org.tros.utils.logging.Logging;
+
+/**
+ *
+ * @author matta
+ */
+public class StackViewTest {
+
+    private final static Logger LOGGER;
+    
+    static {
+        Logging.initLogging(TorgoInfo.INSTANCE);
+        LOGGER = Logger.getLogger(StackViewTest.class.getName());
+    }
+    
+    public StackViewTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class TraceLogger.
+     */
+    @Test
+    public void stackViewLoggerTest() {
+        LOGGER.info("stackViewLoggerTest");
+        final java.util.prefs.Preferences prefs = java.util.prefs.Preferences.userNodeForPackage(LogoMenuBar.class);
+        boolean checked = prefs.getBoolean("wait-for-repaint", true);
+        prefs.putBoolean("wait-for-repaint", true);
+        DynamicLogoController controller = (DynamicLogoController) TorgoToolkit.getController("dynamic-logo");
+        controller.run();
+        assertEquals("dynamic-logo", controller.getLang());
+
+        controller.enable("StackView");
+
+        String[] files = new String[]{
+            "logo/examples/tortue/octagon.logo",
+//            "logo/examples/tortue/pretty.logo",
+//            "logo/examples/tortue/snowflake.logo",
+//            "logo/examples/tortue/spokes.logo",
+            "logo/examples/tortue/test.logo"
+//            "logo/examples/tortue/tortue-text.logo"
+        };
+
+        for (String file : files) {
+            Logger.getLogger(LogoControllerTest.class.getName()).log(Level.INFO, file);
+            controller.openFile(ClassLoader.getSystemClassLoader().getResource(file));
+
+            final AtomicBoolean started = new AtomicBoolean(false);
+            final AtomicBoolean finished = new AtomicBoolean(false);
+            controller.addInterpreterListener(new InterpreterListener() {
+                @Override
+                public void started() {
+                    started.set(true);
+                }
+
+                @Override
+                public void finished() {
+                    finished.set(true);
+                }
+
+                @Override
+                public void error(Exception e) {
+                }
+
+                @Override
+                public void message(String msg) {
+                }
+
+                @Override
+                public void currStatement(CodeBlock block, Scope scope) {
+                }
+            });
+
+            controller.startInterpreter();
+
+            try {
+                while (!finished.get()) {
+                    Thread.sleep(10);
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(LogoControllerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            assertTrue(started.get());
+            assertTrue(finished.get());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(LogoControllerTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        controller.close();
+        prefs.putBoolean("wait-for-repaint", checked);
+    }
+}
