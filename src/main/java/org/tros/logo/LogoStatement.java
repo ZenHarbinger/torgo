734afbc870dfdc1a36e60d69329550bbfdf3c980
Matthew Aguirre <matt.aguirre@gmail.com>
2016-12-30 16:58:36 -0500

    Changed back from lambda.

--- src/main/java/org/tros/logo/LogoStatement.java
+++ src/main/java/org/tros/logo/LogoStatement.java
@@ -250,9 +250,9 @@ class LogoStatement extends LogoBlock {
                         HashMap<String, InterpreterValue> paramValues = new HashMap<>();
 
                         //get the parameter names
-                        funct.parameterDeclarations().forEach((param) -> {
+                        for (LogoParser.ParameterDeclarationsContext param : funct.parameterDeclarations()) {
                             paramNames.add(param.getText().substring(1));
-                        });
+                        }
 
                         LogoParser.ProcedureInvocationContext context = (LogoParser.ProcedureInvocationContext) ctx;
 
