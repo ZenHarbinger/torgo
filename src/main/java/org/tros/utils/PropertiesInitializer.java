b11432431b1e165e09a225e03813ebfc9b4225bd
Matthew Aguirre <matt.aguirre@gmail.com>
2017-01-20 19:17:52 -0500

    Attempt to fix circleci build

--- src/main/java/org/tros/utils/PropertiesInitializer.java
+++ src/main/java/org/tros/utils/PropertiesInitializer.java
@@ -9,9 +9,15 @@ package org.tros.utils;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ResourceBundle;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Properties;
+import org.tros.torgo.TorgoInfo;
 
 /**
  *
@@ -19,32 +25,199 @@ import java.util.ResourceBundle;
  */
 public abstract class PropertiesInitializer {
 
+    private static final Object LOCK = new Object();
     private static final org.tros.utils.logging.Logger LOGGER = org.tros.utils.logging.Logging.getLogFactory().getLogger(PropertiesInitializer.class);
 
+    @SuppressWarnings("OverridableMethodCallInConstructor")
     protected PropertiesInitializer() {
-        this.initializeFromProperties();
+        synchronized (LOCK) {
+            this.initializeHelper();
+
+            String dir = null;
+            if (!BuildInfo.class.isAssignableFrom(this.getClass())) {
+                dir = PathUtils.getApplicationConfigDirectory(TorgoInfo.INSTANCE);
+            }
+
+            this.initializeFromProperties();
+            this.initializeFromProperties(dir);
+        }
+    }
+
+    /**
+     * Initialize from properties file if possible.
+     *
+     * @param dir
+     */
+    private void initializeFromProperties(String dir) {
+        if (dir == null) {
+            return;
+        }
+        Properties prop = new Properties();
+
+        String propFile = dir + '/' + this.getClass().getSimpleName() + ".properties";
+        File f = new File(propFile);
+        if (f.exists()) {
+
+            try (FileInputStream fis = new FileInputStream(f)) {
+                PropertyDescriptor[] props = Introspector.getBeanInfo(this.getClass()).getPropertyDescriptors();
+                prop.load(fis);
+
+                ArrayList<String> propKeys = new ArrayList<>(prop.stringPropertyNames());
+                for (PropertyDescriptor p : props) {
+                    if (p.getWriteMethod() != null
+                            && p.getReadMethod() != null
+                            && p.getReadMethod().getDeclaringClass() != Object.class) {
+                        boolean success = false;
+                        String val = prop.getProperty(p.getName());
+                        if (val != null) {
+                            Object o = TypeHandler.fromString(p.getPropertyType(), val);
+                            if (o != null) {
+                                p.getWriteMethod().invoke(this, o);
+                                success = true;
+                            }
+                        }
+                        if (!success && val != null) {
+//                            if (TypeHandler.isEnumeratedType(p)) {
+////                                setEnumerated(p, val);
+//                            } else {
+                            setValueHelper(p, val);
+//                            }
+                        }
+                    }
+                }
+                propKeys.forEach((key) -> {
+                    String value = prop.getProperty(key);
+                    setNameValuePair(key, value);
+                });
+            } catch (NullPointerException | IOException | IllegalArgumentException | InvocationTargetException ex) {
+            } catch (IllegalAccessException ex) {
+                LOGGER.debug(null, ex);
+            } catch (IntrospectionException ex) {
+                LOGGER.warn(null, ex);
+            }
+        }
     }
 
     /**
      * Initialize from properties file if possible.
      */
     private void initializeFromProperties() {
-        String name = this.getClass().getCanonicalName();
-        name = name.replace(".", "/");
-        ResourceBundle resources = ResourceBundle.getBundle(name);
         try {
-            PropertyDescriptor[] thisProps = Introspector.getBeanInfo(this.getClass()).getPropertyDescriptors();
-            for(PropertyDescriptor pd : thisProps) {
-                 if(resources.containsKey(pd.getName())) {
-                     Method writeMethod = pd.getWriteMethod();
-                     if(writeMethod != null) {
-                         writeMethod.invoke(this, resources.getString(pd.getName()));
-                     }
-                 }
+            Properties prop = new Properties();
+
+            String propFile = this.getClass().getPackage().getName().replace('.', '/') + '/' + this.getClass().getSimpleName() + ".properties";
+            java.util.Enumeration<URL> resources = ClassLoader.getSystemClassLoader().getResources(propFile);
+            ArrayList<URL> urls = new ArrayList<>();
+
+            //HACK: semi sort classpath to put "files" first and "jars" second.
+            //this has an impact once we are workin in tomcat where
+            //the classes in tomcat are not stored in a jar.
+            while (resources.hasMoreElements()) {
+                URL url = resources.nextElement();
+                if (url.toString().startsWith("file:")) {
+                    urls.add(0, url);
+                } else {
+                    boolean added = false;
+                    for (int ii = 0; !added && ii < urls.size(); ii++) {
+                        if (!urls.get(ii).toString().startsWith("file:")) {
+                            urls.add(ii, url);
+                            added = true;
+                        }
+                    }
+                    if (!added) {
+                        urls.add(url);
+                    }
+                }
             }
-        } catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+
+            //reverse the list, so that the item found first in the
+            //classpath will be the last one run though and thus
+            //be the one to set the final value
+            Collections.reverse(urls);
+
+            PropertyDescriptor[] props = Introspector.getBeanInfo(this.getClass()).getPropertyDescriptors();
+            urls.forEach((url) -> {
+                try {
+                    prop.load(url.openStream());
+                    ArrayList<String> propKeys = new ArrayList<>(prop.stringPropertyNames());
+
+                    for (PropertyDescriptor p : props) {
+                        if (p.getWriteMethod() != null
+                                && p.getReadMethod() != null
+                                && p.getReadMethod().getDeclaringClass() != Object.class) {
+                            boolean success = false;
+                            String val = prop.getProperty(p.getName());
+                            if (val != null) {
+                                Object o = TypeHandler.fromString(p.getPropertyType(), val);
+                                if (o != null) {
+                                    p.getWriteMethod().invoke(this, o);
+                                    success = true;
+                                }
+                            }
+                            if (!success && val != null) {
+//                                if (TypeHandler.isEnumeratedType(p)) {
+////                                    success = setEnumerated(p, val);
+//                                } else {
+success = setValueHelper(p, val);
+//                                }
+                            }
+                            if (success) {
+                                propKeys.remove(p.getName());
+                            }
+                        }
+                    }
+                    for (String key : propKeys) {
+                        String value = prop.getProperty(key);
+                        setNameValuePair(key, value);
+                    }
+                } catch (NullPointerException | IOException | IllegalArgumentException | InvocationTargetException ex) {
+                } catch (IllegalAccessException ex) {
+                    LOGGER.warn(null, ex);
+                }
+            });
+        } catch (IOException ex) {
+        } catch (IntrospectionException ex) {
             LOGGER.warn(null, ex);
         }
+    }
+
+    /**
+     * Set values.
+     *
+     * @param p
+     * @param value
+     * @return
+     */
+    protected boolean setValueHelper(PropertyDescriptor p, String value) {
+        return false;
+    }
+
+    /**
+     * Set name value.
+     *
+     * @param name
+     * @param value
+     */
+    protected void setNameValuePair(String name, String value) {
+    }
+
+    /**
+     * helper method for initialization.
+     *
+     * Called during constructor. Derived classes should not initialize anything
+     * in their constructor as that will over-write any values read in from
+     * file.
+     */
+    protected void initializeHelper() {
+    }
+
+    /**
+     * Get a display name.
+     *
+     * @return
+     */
+    public String displayName() {
+        return this.getClass().getCanonicalName();
     }
 
 }
