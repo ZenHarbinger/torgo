015b4cf3364b7356a158832363095e4d565c67a6
samwash <samwash@vt.edu>
2017-07-07 20:39:43 -0400

    new test class for TorgoUserInputPanel

---
+++ src/test/java/org/tros/torgo/swing/TorgoUserInputPanelTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015-2017 Matthew Aguirre
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tros.torgo.swing;
+
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.tros.torgo.TorgoInfo;
+import org.tros.utils.logging.Logging;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.tros.logo.DynamicLogoController;
+/**
+ *
+ * @author Samuel Washburn
+ */
+public class TorgoUserInputPanelTest {
+    private final static Logger LOGGER;
+    
+    static {
+        Logging.initLogging(TorgoInfo.INSTANCE);
+        LOGGER = Logger.getLogger(TorgoToolBarTest.class.getName());
+    }
+
+    public TorgoUserInputPanelTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testZoomIn() {
+        TorgoUserInputPanel panel = new TorgoUserInputPanel(new DynamicLogoController(), "Logo", false, "text/logo");
+        panel.zoomInTest();
+        assertEquals(panel.getInputTextAreaSize(), 12);
+    }
+
+    @Test
+    public void testZoomOut() {
+        TorgoUserInputPanel panel = new TorgoUserInputPanel(new DynamicLogoController(), "Logo", false, "text/logo");
+        panel.zoomOutTest();
+        assertEquals(panel.getInputTextAreaSize(), 11);
+    }
+
+    @Test
+    public void testZoomReset() {
+        TorgoUserInputPanel panel = new TorgoUserInputPanel(new DynamicLogoController(), "Logo", false, "text/logo");
+        panel.zoomResetTest();
+        assertEquals(panel.getInputTextAreaSize(), 12);
+    }
+
+    @Test
+    public void testAppendToSource() {
+        TorgoUserInputPanel panel = new TorgoUserInputPanel(new DynamicLogoController(), "Logo", true, "text/logo");
+        panel.appendToSource("test");
+        assertTrue(panel.getInputTextArea().getText().contains("test"));
+    }
+}
