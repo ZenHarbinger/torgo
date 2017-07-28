bba66691d02c934c6f303aed4ffc3f909783c5e0
DangTimmyTD <DangTimmyTD@gmail.com>
2017-07-27 20:32:14 -0400

    Fixing error

--- src/test/java/org/tros/utils/PropertiesInitializerTest.java
+++ src/test/java/org/tros/utils/PropertiesInitializerTest.java
@@ -1,64 +1,60 @@
-///*
-// * Copyright 2015-2017 Matthew Aguirre
-// * 
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// * 
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// * 
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//package org.tros.utils;
-//
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Assert;
-//import static org.junit.Assert.assertNotNull;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import org.tros.torgo.TorgoInfo;
-//
-///**
-// *
-// * @author matta
-// */
-//public class PropertiesInitializerTest {
-//    
-//    public PropertiesInitializerTest() {
-//    }
-//    
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//    
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//    
-//    @Before
-//    public void setUp() {
-//    }
-//    
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of canCopy method, of class PropertiesInitializer.
-//     */
-//    @Test
-//    public void testCopy() {
-//        TorgoInfo info = new TorgoInfo();
-//        TorgoInfo info2 = (TorgoInfo)info.copy();
-//        assertNotNull(info2);
-//        Assert.assertNotEquals(info, info2);
-//        info2.copy(info);
-//    }
-//    
-//}
+/*
+ * Copyright 2015-2017 Matthew Aguirre
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http:www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tros.utils;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.tros.torgo.TorgoInfo;
+
+/**
+ *
+ * @author matta
+ */
+public class PropertiesInitializerTest {
+    
+    public PropertiesInitializerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of canCopy method, of class PropertiesInitializer.
+     */
+    @Test
+    public void testCopy() {
+        //PropertiesInitializer prop = new PropertiesInitializer();
+    }
+    
+}
